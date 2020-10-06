a5c4c1d5a27ebbb267f4aa154de49e8e1fab4497
Khushbu Patel <kathrotiyakhushbu11@gmail.com>
2020-10-06 13:53:04 +0530

    First Commit uploading all the files

---
+++ Notepad/src/Keyhandler.java
@@ -0,0 +1,35 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Keyhandler implements KeyListener {
+	GUI gui;
+	 public Keyhandler(GUI gui) {
+		 this.gui = gui;
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.isControlDown() && e.getKeyCode() == KeyEvent.VK_S){
+			gui.file.save();
+		}
+		
+		if(e.isShiftDown() && e.isControlDown() && e.getKeyCode() == KeyEvent.VK_S){
+			gui.file.saveAs();
+		}
+		
+		if(e.isAltDown() &&  e.getKeyCode() == KeyEvent.VK_F){
+			gui.menuFile.doClick();
+		}
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
